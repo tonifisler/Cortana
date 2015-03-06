7fbc5f8d03c89ce796e1319047477d9b49cd7152
Toni Fisler <mail@tonifisler.ch>
2015-03-06 14:33:39 +0100

    add script to add class to tables in cortana-content

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,7 @@
     $(this).find($output).append('<button type="button" class="btn btn-xs btn-info show-code" data-toggle="collapse" data-target="#codeBlock-' + codeIndex + '">&lt;/&gt;</button>');
     $(this).find($markup).addClass('collapse').attr('id', 'codeBlock-' + codeIndex);
   });
+
+  $('.cortana-content > table').addClass('table table-bordered');
+
 }) (jQuery_no_conflict);
